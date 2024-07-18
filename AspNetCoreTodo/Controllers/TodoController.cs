ca9e364119b8c9ac0a5938355ddb1785e8513190
jbowen999 <julian.e.bowen@outlook.com>
2024-07-17 18:34:44 -0700

    controller finished

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
 using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
 namespace AspNetCoreTodo.Controllers
@@ -21,6 +22,13 @@ public async Task<IActionResult> Index()
 // Get to-do items from database
 // Put items into a model
 // Pass the view to a model and render
+  var model = new TodoViewModel
+        {
+            Items = items // Initialize list of to-do items
+        };
+
+        // Render view using the model, passing the populated view model to the view
+        return View(model);
 }
 }
 }
