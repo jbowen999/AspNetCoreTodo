c8840d26827326f26506306bbf137ff49e2a1765
jbowen999 <julian.e.bowen@outlook.com>
2024-07-22 09:46:11 -0700

    chhay is the best

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -7,28 +7,57 @@ using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
 namespace AspNetCoreTodo.Controllers
 {
-    
-public class TodoController : Controller
-{
-private readonly ITodoItemService _todoItemService;
-public TodoController(ITodoItemService todoItemService)
-{
-_todoItemService = todoItemService;
-}
-public async Task<IActionResult> Index()
-{
-    var items = await _todoItemService.GetIncompleteItemsAsync();
-    
-// Get to-do items from database
-// Put items into a model
-// Pass the view to a model and render
-  var model = new TodoViewModel
-        {
-            Items = items // Initialize list of to-do items
-        };
 
-        // Render view using the model, passing the populated view model to the view
-        return View(model);
-}
-}
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+
+            // Get to-do items from database
+            // Put items into a model
+            // Pass the view to a model and render
+            var model = new TodoViewModel
+            {
+                Items = items // Initialize list of to-do items
+            };
+
+            // Render view using the model, passing the populated view model to the view
+            return View(model);
+        }
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddItem(TodoItem newItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            var successful = await _todoItemService.AddItemAsync(newItem);
+            if (!successful)
+            {
+                return BadRequest("Could not add item.");
+            }
+            return RedirectToAction("Index");
+        }
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MarkDone(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return RedirectToAction("Index");
+            }
+            var successful = await _todoItemService.MarkDoneAsync(id);
+            if (!successful)
+            {
+                return BadRequest("Could not mark item as done.");
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
 }
--- AspNetCoreTodo/Services/ITodoItemService.cs
+++ AspNetCoreTodo/Services/ITodoItemService.cs
@@ -4,8 +4,10 @@ using System.Threading.Tasks;
 using AspNetCoreTodo.Models;
 namespace AspNetCoreTodo.Services
 {
-public interface ITodoItemService
-{
-Task<TodoItem[]> GetIncompleteItemsAsync();
-}
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync();
+        Task<bool> AddItemAsync(TodoItem newItem);
+        Task<bool> MarkDoneAsync(Guid id);
+    }
 }
